a57011ba047761722292a8591a4ca0eb0bef34b7
Matthew Riley <matthewriley@interapts-macbook-pro-10.local>
2018-08-30 09:29:16 -0500

    Initial Commit

---
+++ VideoGameLibrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VideoGameLibrary
+//
+//  Created by Matthew Riley on 8/30/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
